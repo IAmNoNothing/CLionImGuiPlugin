162a02c56db85c86daf1629ea1f122d1603dd675
GitHub Action <action@github.com>
2026-07-31 09:26:32 +0000

    Template cleanup

--- src/main/kotlin/com/github/iamnonothing/clionimguiplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/iamnonothing/clionimguiplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.iamnonothing.clionimguiplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.iamnonothing.clionimguiplugin.MyBundle
+import com.github.iamnonothing.clionimguiplugin.services.MyProjectService
 import javax.swing.JButton
 
 
